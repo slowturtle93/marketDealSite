72bb3985a10686466d6b7a37d345e16ac9effa91
haksongKim <gkrthdz@naver.com>
2021-05-12 10:25:21 +0900

    # 프로젝트 초기 생성

---
+++ src/main/java/com/market/server/MartketServerApplication.java
@@ -0,0 +1,13 @@
+package com.market.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartketServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MartketServerApplication.class, args);
+	}
+
+}
