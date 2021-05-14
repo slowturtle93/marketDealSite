122e60a7183ae5082bd23f23b4bc006697db2666
gkrthdz <gkrthdz@naver.com>
2021-05-14 10:09:31 +0900

    # 프로젝트 초기 생성

--- src/main/java/com/market/server/MartketServerApplication.java
+++ src/main/java/com/market/server/MartketServerApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MartketServerApplication {
 
-	
 	public static void main(String[] args) {
 		SpringApplication.run(MartketServerApplication.class, args);
 	}
